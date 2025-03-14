2c646430bbe7c8c12bcd365a888f16d1d29048a7
Saleha Khan <161796561+Ibrahimsabir@users.noreply.github.com>
2025-03-14 23:19:33 +0500

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import re
+
+st.set_page_config(page_title="Password Strength Checker" , page_icon="🔒")
+
+st.title("🔐 Password Strength Checker")
+st.markdown("""## Welcome to ultimate password strength checker! Use this simple tool to check the strength of your password and get suggestions on how to make your password more secure. We will give you helpful tips to create a **Strong Password** """)
